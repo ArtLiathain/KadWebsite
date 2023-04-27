98b94854fec41fee362d60812dd25625742658dc
ArtLiathain <art.oliathain@gmail.com>
2023-04-27 22:35:38 +0100

    Add failing test for CI and add linter/formatter

--- app/src/test/java/com/example/kad/MainActivityTest.java
+++ app/src/test/java/com/example/kad/MainActivityTest.java
@@ -1,18 +1,20 @@
 package com.example.kad;
-import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Test;
+
 public class MainActivityTest {
-    @Test
-    public void sumTest() {
-        logic1 logic1 = new logic1();
+  @Test
+  public void sumTest() {
+    logic1 logic1 = new logic1();
 
-        // Test case 1: Single item
+    // Test case 1: Single item
+    assertEquals(20, logic1.sum(20, 2));
+  }
 
-        int result1 = logic1.sum(20,2);
-        assertEquals(22, result1);
-
-
-    }
+  @Test
+  public void trueTest() {
+    assertEquals(true, 1);
+  }
 }

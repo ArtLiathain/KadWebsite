d1066291859dcea2090d8c62697a9c030e1b1322
ArtLiathain <art.oliathain@gmail.com>
2023-04-27 15:44:50 +0100

    Revert "no"
    This reverts commit 093885610f5e75c2fcb5435fc3713302817dba1e.

--- app/src/main/java/com/example/kad/MainActivity.java
+++ app/src/main/java/com/example/kad/MainActivity.java
@@ -2,38 +2,13 @@ package com.example.kad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn1, btn2, btn3,btn4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btn1 = findViewById(R.id.button);
-
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                Toast.makeText(MainActivity.this, "button clicked", Toast.LENGTH_SHORT).show();
-                    testFun();
-            }
-        });
     }
-
-
-    public boolean testFun(){
-        return true;
-
-
-        //hello
-    }
-
 }

1264a2722829ed0090ebbef628ecb1aa2ceb3126
Karl Gilmartin <karl_gilmartin@hotmail.com>
2023-04-28 11:32:44 +0100

    Test for auth page

--- app/src/main/java/com/example/kad/logic1.java
+++ app/src/main/java/com/example/kad/logic1.java
@@ -1,7 +1,81 @@
 package com.example.kad;
 
 public class logic1 {
+
     public int sum(int num1, int num2){
         return (num1+num2);
+    }
+
+    public boolean name_length(String email){
+        if (email.length()<= 70 & email.length() > 0){
+            return true;
+        }
+        return false;
+
+    }
+    public boolean string_alphabet(String name){
+        return name.matches("^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$");
+//        it was [a-zA-Z]+
+    }
+
+//    public first_last(String){
+//        // ensures that there is two words, first and last name
+//    }
+
+    public boolean blacklist(String name){
+        int count = 0;
+        String[] blackList = {"John Smith", "Jane Doe", "Michael Johnson", "Emily Brown", "William Davis", "Olivia Taylor", "James Wilson", "Sophia Lee", "Benjamin Garcia", "Isabella Martinez", "Daniel Anderson", "Mia Thomas", "David Hernandez", "Ella Perez", "Joseph Green", "Ava Collins", "Matthew Baker", "Chloe Hall", "Andrew Rivera", "Natalie Ramirez"};
+        for(int i =0; i<= blackList.length -1;i++){
+            if (blackList[i] == name){
+                count += 1;
+            }
+        }
+        if (count == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+
+
+
+
+
+    ///////////////////////////////////////////////////////////
+//                             Email
+    ///////////////////////////////////////////////////////////
+    public boolean email_format(String email){
+        return email.matches("^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$");
+    }
+
+
+
+
+
+
+
+
+
+    ///////////////////////////////////////////////////////////
+//                             Student Number
+    ///////////////////////////////////////////////////////////
+
+    public static boolean student_num_len(int number){
+        int length = (int) (Math.log10(number) + 1);
+        if (length !=8){
+            return false;
+        }
+        return true;
+    }
+    public static boolean student_num_year(int number){
+        if (number >= 22000000){
+            return true;
+        }
+        else {
+            return false;
+        }
+
     }
 }
--- app/src/test/java/com/example/kad/MainActivityTest.java
+++ app/src/test/java/com/example/kad/MainActivityTest.java
@@ -5,14 +5,29 @@ import static org.junit.Assert.*;
 
 public class MainActivityTest {
     @Test
-    public void sumTest() {
+    public void name() {
         logic1 logic1 = new logic1();
-
         // Test case 1: Single item
+        assertEquals(true, logic1.name_length("Karl Gilmartin"));
+        assertEquals(true, logic1.name_length("k"));
 
-        int result1 = logic1.sum(20,2);
-        assertEquals(22, result1);
+
+        assertEquals(true, logic1.string_alphabet("Brid O'Dea"));
+        assertEquals(false, logic1.string_alphabet("Karl Gilmartín"));
+        // this is false, the current system doesn't accept "í"
+
+        assertEquals(false, logic1.blacklist("Andrew Rivera"));
+        assertEquals(true, logic1.blacklist("Karl Gilmartín"));
 
 
+    }
+    @Test
+    public void student_num_len(){
+        assertEquals(true, logic1.student_num_len(22349111));
+
+    }
+    @Test
+    public void student_num_year(){
+        assertEquals(false, logic1.student_num_year(21349111));
     }
 }

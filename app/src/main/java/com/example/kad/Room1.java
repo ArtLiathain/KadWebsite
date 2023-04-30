9bc5bc126e285266c4c5f23490cc65676ef79ebc
Darragh-Grealish <dgrealish2003@gmail.com>
2023-04-30 18:42:26 +0100

    Added Calander

--- app/src/main/java/com/example/kad/Room1.java
+++ app/src/main/java/com/example/kad/Room1.java
@@ -1,67 +1,73 @@
 package com.example.kad;
 
-import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.AdapterView;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.Spinner;
 import android.widget.Toast;
-
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class Room1 extends AppCompatActivity {
     Spinner attendeesNumber;
 
-    // create array of Strings
-    // and store name of courses
-    Integer[] number_of_attendees = { 1, 2, 3, 4, 5, 6, 7, 9, 10 };
-    
+    DatePicker datePicker;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.room_1);
 
+// ---------------------------------Number of Attendees---------------------------------
+
         attendeesNumber = findViewById(R.id.attendees_spinner);
-        ArrayAdapter ad = new ArrayAdapter(this, android.R.layout.simple_spinner_item, number_of_attendees);
+        // Retrieve a list of available times from a data source
+        List<Integer> numberOfAttendees = numberOfAttendees();
+        ArrayAdapter ad = new ArrayAdapter(this, android.R.layout.simple_spinner_item, numberOfAttendees);
 
         // Styles dropdown of numbers nicely
         ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Toast.makeText(Room1.this, "The Calendar Works", Toast.LENGTH_SHORT).show();
         attendeesNumber.setAdapter(ad);
-    }
 
+// ------------------------------------------------------------------------
 
-    /*
-    @Override
-    public void onItemSelected(AdapterView<*> arg0, View arg1, int position, long id)
-    {
-
-        // make toastof name of course
-        // which is selected in spinner
-        Toast.makeText(getApplicationContext(), courses[position], Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<*> arg0)
-    {
-        // Auto-generated method stub
-    }
-    */
-
-        /*
-        attendeesNumber.setOnClickListener(new View.OnClickListener() {
-
+        attendeesNumber.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout., new String[]{"Option 1", "Option 2", "Option 3"});
-                Spinner spinner = null;
-                spinner.setAdapter(adapter);
-
-                Toast.makeText(Room1.this, "The Room1 Button Works", Toast.LENGTH_SHORT).show();
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = parent.getItemAtPosition(position).toString();
+                Toast.makeText(getApplicationContext(), "The Selected number of Attendees -> " + selectedItem, Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Do nothing
             }
         });
-        */
         
+
+// ---------------------------------Date Picker---------------------------------
+
+        datePicker = findViewById(R.id.datePicker1);
+        datePicker.setOnDateChangedListener(new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Toast.makeText(Room1.this, "Calendar Works " + dayOfMonth + ' ' + monthOfYear + ' ' + year, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+    private List<Integer> numberOfAttendees() {
+        return Arrays.asList(1, 2, 3, 4, 5);
+        // "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM"
+    }
 
 }

b65afd5cf5207d2437997f4dfdb934fcac6f1c88
ArtLiathain <art.oliathain@gmail.com>
2023-04-30 22:03:22 +0100

    Add Parameterised testing and fixed logic code

--- app/src/main/java/com/example/kad/DateTimeLogic.java
+++ app/src/main/java/com/example/kad/DateTimeLogic.java
@@ -3,12 +3,12 @@ import java.time.LocalDateTime;
 
 public class DateTimeLogic {
 
-	public boolean isOverLapping(LocalDateTime date1,int timeBooked, LocalDateTime date2, int hoursBooked){
-		if (date1.isBefore(date2) && date1.plusHours(timeBooked).isBefore(date2)){
-			if(date1.isAfter(date2) && date1.isAfter(date2.plusHours(hoursBooked))){
-				return false;
-			}
+	public boolean isOverLapping(LocalDateTime date1,int minsToBook, LocalDateTime date2, int minutesBooked){
+		if ((date1.isBefore(date2) && date1.plusMinutes(minsToBook).isBefore(date2)) || (date1.isAfter(date2) && date1.isAfter(date2.plusMinutes(minutesBooked)))){
+			return false;
 		}
+
+
 		return true;
 	}
 }
--- app/src/test/java/com/example/kad/DateTimeTest.java
+++ app/src/test/java/com/example/kad/DateTimeTest.java
@@ -1,27 +1,63 @@
 package com.example.kad;
 
-import org.junit.Assert;
-import org.junit.Test;
-import java.time.LocalDateTime;
-
-
 import static org.junit.Assert.*;
 
-
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Collection;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
 public class DateTimeTest {
 
-	@Test
-	public void test_isOverLapping(){
-		DateTimeLogic dateTimeLogic = new DateTimeLogic();
-		String bookingDate = "2005-08-20 10:00";
-		String bookedDate = "2005-08-20 09:30";
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-		LocalDateTime formattedBookingDate = LocalDateTime.parse(bookingDate, formatter);
-		LocalDateTime formattedBookedDate = LocalDateTime.parse(bookedDate, formatter);
-		assertEquals(dateTimeLogic.isOverLapping(formattedBookingDate,1, formattedBookedDate, 2), true);
-	}
+  private String bookedDate;
+  private int minsToBook;
+  private String bookingDate;
+  private int minutesBooked;
+
+  private Integer inputNumber;
+  private Boolean expectedResult;
+  private DateTimeLogic dateTimeLogic;
+
+  @Before
+  public void initialize() {
+    dateTimeLogic = new DateTimeLogic();
+  }
+
+  // Each parameter should be placed as an argument here
+  // Every time runner triggers, it will pass the arguments
+  // from parameters we defined in primeNumbers() method
+
+  public DateTimeTest(String bookingDate,int minsToBook, String bookedDate, int minutesBooked, boolean expectedResult) {
+    this.bookingDate = bookingDate;
+    this.minsToBook = minsToBook;
+    this.bookedDate = bookedDate;
+    this.minutesBooked = minutesBooked;
+    this.expectedResult = expectedResult;
+  }
+
+  @Parameterized.Parameters
+  public static Collection dates() {
+    return Arrays.asList(new Object[][] {
+            {"2005-08-20 10:00", 60, "2005-08-20 09:30", 120, true},
+            {"2005-08-20 10:00", 29, "2005-08-20 09:30", 29, false},
+
+    });
+  }
+  @Test
+  public void test_isOverLapping() {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    LocalDateTime formattedBookingDate = LocalDateTime.parse(bookingDate, formatter);
+    LocalDateTime formattedBookedDate = LocalDateTime.parse(bookedDate, formatter);
+    assertEquals(
+        dateTimeLogic.isOverLapping(formattedBookingDate, minsToBook, formattedBookedDate, minutesBooked), expectedResult);
+  }
+
 
 
 }

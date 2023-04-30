2385bc66c58f27ce10f6713c700f4e5ab4e02cb1
ArtLiathain <art.oliathain@gmail.com>
2023-04-30 14:49:10 +0100

    Test coveragpls work

--- app/src/test/java/com/example/kad/MainActivityTest.java
+++ app/src/test/java/com/example/kad/MainActivityTest.java
@@ -1,16 +1,16 @@
-//package com.example.kad;
-//
-//import static org.junit.Assert.*;
-//
-//import org.junit.Test;
-//
-//public class MainActivityTest {
-//  @Test
-//  public void sumTest() {
-//
-//    logic1 logic1 = new logic1();
-//
-//    // Test case 1: Single item
-//    assertEquals(22, logic1.sum(20, 2));
-//  }
-//}
+package com.example.kad;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainActivityTest {
+  @Test
+  public void sumTest() {
+
+    logic1 logic1 = new logic1();
+
+    // Test case 1: Single item
+    assertEquals(22, logic1.sum(20, 2));
+  }
+}

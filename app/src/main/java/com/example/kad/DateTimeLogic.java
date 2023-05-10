347fbc04123f3d7e0100f035ca6795b277957040
ArtLiathain <art.oliathain@gmail.com>
2023-05-10 22:24:51 +0100

    failing test with correct input

--- app/src/main/java/com/example/kad/DateTimeLogic.java
+++ app/src/main/java/com/example/kad/DateTimeLogic.java
@@ -2,13 +2,10 @@ package com.example.kad;
 import java.time.LocalDateTime;
 
 public class DateTimeLogic {
-
-	public boolean isOverLapping(LocalDateTime date1,int minsToBook, LocalDateTime date2, int minutesBooked){
-		if ((date1.isBefore(date2) && date1.plusMinutes(minsToBook).isBefore(date2)) || (date1.isAfter(date2) && date1.isAfter(date2.plusMinutes(minutesBooked)))){
-			return false;
-		}
-
-
-		return true;
+	Firebase firebase = new Firebase();
+	public String[] ReturnStartDates(){
+		String[] temp;
+		temp = new String[]{"1", "2"};
+		return temp;
 	}
 }

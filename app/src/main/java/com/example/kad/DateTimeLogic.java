011a1a52c814d76305fb813caf2036a1f2d37f6a
ArtLiathain <art.oliathain@gmail.com>
2023-05-10 23:01:00 +0100

    Add firebase testing and modify build gradle
    need to comment out tests for now to pass builds must ask for more help
    with firebase before proceeding

--- app/src/main/java/com/example/kad/DateTimeLogic.java
+++ app/src/main/java/com/example/kad/DateTimeLogic.java
@@ -1,11 +1,38 @@
 package com.example.kad;
-import java.time.LocalDateTime;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class DateTimeLogic {
+
+	ArrayList<String> availableTimes = new ArrayList<>(List.of("Not Selected", "09:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "01:00 PM", "02:00 PM", "03:00 PM", "04:00 PM"));
 	Firebase firebase = new Firebase();
-	public String[] ReturnStartDates(){
-		String[] StartDates;
-		StartDates = firebase.getDates();
-		return temp;
+	public String[] returnStartTimes(){
+		List<Date> StartDates;
+		firebase.getDates();
+		StartDates = firebase.getStartdates();
+		if (StartDates.size() == 0){
+			return availableTimes.toArray(String[]::new);
+		}
+		int year = StartDates.get(0).getYear();
+		int month = StartDates.get(0).getMonth();
+		ArrayList<String> availableTimesCopy = availableTimes;
+	    for (int i = 1; i < availableTimesCopy.size(); i++) {
+	      try {
+	        Date dateToCheck = new SimpleDateFormat("dd/MM/yyyy").parse(availableTimesCopy.get(i).substring(0,1) + "/" + month + "/" + year);
+			for (int h = 0; h < StartDates.size(); h++) {
+			    if(dateToCheck.equals(StartDates.get(h))){
+					availableTimesCopy.remove(h);
+			    }
+			}
+	      } catch (ParseException e) {
+	        throw new RuntimeException(e);
+	      }
+			}
+
+
+		return availableTimesCopy.toArray(String[]::new);
 	}
 }
--- app/src/test/java/com/example/kad/DateTimeTest.java
+++ app/src/test/java/com/example/kad/DateTimeTest.java
@@ -22,7 +22,7 @@ public class DateTimeTest {
     DateTimeLogic dateTimeLogic = new DateTimeLogic();
     String[] times = new String[]{"Not Selected", "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM"};
     assertArrayEquals(
-        dateTimeLogic.ReturnStartDates(), times);
+        dateTimeLogic.returnStartTimes(), times);
   }
 
 

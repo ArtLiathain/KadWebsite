215e8ab9d1e64a9b4bbc522a3b3814c2a189cd0f
ArtLiathain <art.oliathain@gmail.com>
2023-05-11 10:41:30 +0100

    Api passes path but only has 2022 holidays

--- app/src/main/java/com/example/kad/DateTimeLogic.java
+++ app/src/main/java/com/example/kad/DateTimeLogic.java
@@ -1,11 +1,19 @@
 package com.example.kad;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import com.github.agogs.holidayapi.api.APIConsumer;
+import com.github.agogs.holidayapi.api.impl.HolidayAPIConsumer;
+import com.github.agogs.holidayapi.model.Holiday;
+import com.github.agogs.holidayapi.model.HolidayAPIResponse;
+import com.github.agogs.holidayapi.model.QueryParams;
+
+import java.io.IOException;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Date;
+
 import java.util.List;
+
+
 
 public class DateTimeLogic {
 
@@ -21,37 +29,71 @@ public class DateTimeLogic {
               "02:00 PM",
               "03:00 PM",
               "04:00 PM"));
-  Firebase firebase = new Firebase();
+//  Firebase firebase = new Firebase();
+//
+//  public String[] returnStartTimes() {
+//    List<Date> StartDates;
+//    firebase.getDates();
+//    StartDates = firebase.getStartdates();
+//    if (StartDates.size() == 0) {
+//      return availableTimes.toArray(String[]::new);
+//    }
+//    int year = StartDates.get(0).getYear();
+//    int month = StartDates.get(0).getMonth();
+//    ArrayList<String> availableTimesCopy = availableTimes;
+//    for (int i = 1; i < availableTimesCopy.size(); i++) {
+//      try {
+//        Date dateToCheck =
+//            new SimpleDateFormat("dd/MM/yyyy")
+//                .parse(availableTimesCopy.get(i).substring(0, 1) + "/" + month + "/" + year);
+//        for (int h = 0; h < StartDates.size(); h++) {
+//          if (dateToCheck.equals(StartDates.get(h))) {
+//            availableTimesCopy.remove(h);
+//          }
+//        }
+//      } catch (ParseException e) {
+//        throw new RuntimeException(e);
+//      }
+//    }
+//
+//    return availableTimesCopy.toArray(String[]::new);
+//  }
 
-  public String[] returnStartTimes() {
-    List<Date> StartDates;
-    firebase.getDates();
-    StartDates = firebase.getStartdates();
-    if (StartDates.size() == 0) {
-      return availableTimes.toArray(String[]::new);
-    }
-    int year = StartDates.get(0).getYear();
-    int month = StartDates.get(0).getMonth();
-    ArrayList<String> availableTimesCopy = availableTimes;
-    for (int i = 1; i < availableTimesCopy.size(); i++) {
-      try {
-        Date dateToCheck =
-            new SimpleDateFormat("dd/MM/yyyy")
-                .parse(availableTimesCopy.get(i).substring(0, 1) + "/" + month + "/" + year);
-        for (int h = 0; h < StartDates.size(); h++) {
-          if (dateToCheck.equals(StartDates.get(h))) {
-            availableTimesCopy.remove(h);
-          }
+  public boolean isHoliday(LocalDateTime stevensDay) {
+    APIConsumer consumer = new HolidayAPIConsumer("https://holidayapi.com/v1/holidays");
+
+    QueryParams params = new QueryParams();
+    params.key("97231d0e-86f6-4f8f-adb5-6567be20b20c")
+            .day(stevensDay.getDayOfMonth())
+            .month(stevensDay.getMonthValue())
+            .country(QueryParams.Country.IRELAND)
+            .year(2022);
+
+    try {
+      //make the API call
+      HolidayAPIResponse response = consumer.getHolidays(params);
+
+      //check the status code of the API call
+      int status = response.getStatus();
+
+      if (status != 200) {
+        return false;
+        //handle error scenario
+
+      } else {
+
+        //handle success scenario
+
+        List<Holiday> holidays = response.getHolidays();
+        if (holidays.size() > 0){
+          return true;
         }
-      } catch (ParseException e) {
-        throw new RuntimeException(e);
+
       }
+    } catch (IOException e) {
+      //handle exception
+      return false;
     }
-
-    return availableTimesCopy.toArray(String[]::new);
-  }
-
-  public boolean isHoliday(LocalDateTime date) {
     return false;
   }
 }
--- app/src/test/java/com/example/kad/DateTimeTest.java
+++ app/src/test/java/com/example/kad/DateTimeTest.java
@@ -2,6 +2,7 @@ package com.example.kad;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import org.junit.Test;
@@ -11,20 +12,20 @@ public class DateTimeTest {
 
   //Should return all the valid times for the day
   //unsure of what input is currently
-  @Test
-  public void test_returnStartTimes() {
-    DateTimeLogic dateTimeLogic = new DateTimeLogic();
-    String[] times = new String[]{"Not Selected", "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM"};
-    assertArrayEquals(
-        dateTimeLogic.returnStartTimes(), times);
-  }
+//  @Test
+//  public void test_returnStartTimes() {
+//    DateTimeLogic dateTimeLogic = new DateTimeLogic();
+//    String[] times = new String[]{"Not Selected", "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM"};
+//    assertArrayEquals(
+//        dateTimeLogic.returnStartTimes(), times);
+//  }
 
   @Test
-  public void test_isHolidayApiWorking(){
+  public void test_isHolidayApiWorking() throws IOException {
     DateTimeLogic dateTimeLogic = new DateTimeLogic();
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-    LocalDateTime ChristmasDay = LocalDateTime.parse("2003-12-25 00:00", formatter);
-    assertTrue(dateTimeLogic.isHoliday(ChristmasDay));
+    LocalDateTime StevensDay = LocalDateTime.parse("2003-12-26 00:00", formatter);
+    assertTrue(dateTimeLogic.isHoliday(StevensDay));
   }
 
 

f4292725d56e755fa33aa7c8b9174bd3c466d769
Darragh-Grealish <dgrealish2003@gmail.com>
2023-05-12 14:01:52 +0100

    Remove code smells from DateTimeLogic

--- app/src/main/java/com/example/kad/DateTimeLogic.java
+++ app/src/main/java/com/example/kad/DateTimeLogic.java
@@ -8,12 +8,9 @@ import com.github.agogs.holidayapi.model.QueryParams;
 
 import java.io.IOException;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
-import java.util.Date;
 import java.util.List;
 
 
@@ -32,35 +29,7 @@ public class DateTimeLogic {
               "02:00 PM",
               "03:00 PM",
               "04:00 PM"));
-//  Firebase firebase = new Firebase();
-////
-//  public String[] returnStartTimes() {
-//    List<Date> StartDates;
-//    firebase.getDates();
-//    StartDates = firebase.getStartdates();
-//    if (StartDates.size() == 0) {
-//      return availableTimes.toArray(String[]::new);
-//    }
-//    int year = StartDates.get(0).getYear();
-//    int month = StartDates.get(0).getMonth();
-//    ArrayList<String> availableTimesCopy = availableTimes;
-//    for (int i = 1; i < availableTimesCopy.size(); i++) {
-//      try {
-//        Date dateToCheck =
-//            new SimpleDateFormat("dd/MM/yyyy")
-//                .parse(availableTimesCopy.get(i).substring(0, 1) + "/" + month + "/" + year);
-//        for (int h = 0; h < StartDates.size(); h++) {
-//          if (dateToCheck.equals(StartDates.get(h))) {
-//            availableTimesCopy.remove(h);
-//          }
-//        }
-//      } catch (ParseException e) {
-//        throw new RuntimeException(e);
-//      }
-//    }
-//
-//    return availableTimesCopy.toArray(String[]::new);
-//  }
+
 
   public boolean isHoliday(LocalDateTime stevensDay) {
     APIConsumer consumer = new HolidayAPIConsumer("https://holidayapi.com/v1/holidays");
@@ -88,15 +57,11 @@ public class DateTimeLogic {
         //handle success scenario
 
         List<Holiday> holidays = response.getHolidays();
-        if (holidays.size() > 0){
-          return true;
-        }
-
+          return (holidays.isEmpty());
       }
     } catch (IOException e) {
       //handle exception
       return false;
     }
-    return false;
   }
 }

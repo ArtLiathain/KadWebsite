70f780e94a197dd53180361e017ae2cc26652203
Karl-Gilmartin <karl_gilmartin@hotmail.com>
2023-05-13 21:58:45 +0200

    Auth Log Unit Test complete with MainActivityTest.java

--- app/src/test/java/com/example/kad/MainActivityTest.java
+++ app/src/test/java/com/example/kad/MainActivityTest.java
@@ -1,39 +1,23 @@
 package com.example.kad;
 
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import org.junit.Test;
-import org.junit.Assert;
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Assert;
+import org.junit.Test;
 
 
 public class MainActivityTest  {
     AuthLogic authLogic = new AuthLogic();
-
-    FirebaseFirestore db;
-    //    @Before
-//    public void setUp() throws Exception {
-//        FirebaseOptions options = new FirebaseOptions.Builder()
-//                .setApplicationId()
-//                .setApiKey()
-//                .setDatabaseUrl("https://kad-testing.firebaseio.com")
-//                .build();
-//        FirebaseApp.initializeApp(options);
-//        db = FirebaseFirestore.getInstance();
-//    }
     @Test
     public void test_nameLength() {
-
-        // Test case 1: Single item
         assertEquals(true, authLogic.validLength("Karl Gilmartin"));
-        assertEquals(true, authLogic.validLength("K"));
+        assertEquals(false, authLogic.validLength("K")); // Name !> 5
     }
 
     @Test
     public void name_chars() {
-        assertEquals(false, authLogic.stringAlphabet("Brid O'Dea"));
-        assertEquals(false, authLogic.stringAlphabet("Karl Gilmartín"));
-//         this is false, the current system doesn't accept "í"
+        assertEquals(true, authLogic.stringAlphabet("Eoin O'Brien")); // Regex allows " ' "
+        assertEquals(true, authLogic.stringAlphabet("Karl Gilmartín")); // Regex allows names with faddas
     }
 
     @Test
@@ -46,37 +30,33 @@ public class MainActivityTest  {
 
     @Test
     public void student_num_len() {
-        assertEquals(true, authLogic.studentNumLen(22349111));
+        assertEquals(true, authLogic.studentNumLen(22349111)); // 8 numbers- pass
+        assertEquals(true, authLogic.studentNumLen(1));// 1 number-fail
 
     }
 
     @Test
     public void student_num_year() {
-        assertEquals(false, authLogic.studentNumYear(21349111));
+        assertEquals(false, authLogic.studentNumYear(21349111)); // Starts with 21
+        assertEquals(true, authLogic.studentNumYear(22349111)); // Stars with 22
     }
-
-
-//    @Test
-//    public void testCheckBox() {
-//        // Create a mock CheckBox
-//        MainActivity dummyV = mock(MainActivity.class);
-//        assertEquals(true,dummyV.checkBox(checkBox));
-//    }
 
 
 
     @Test
     public void testColorAccessibility() {
-        int[] color1 = {255, 255, 255}; // White color
-        int[] color2 = {255, 255, 255}; // Black color
-        boolean result = authLogic.colorAccessibility(color1, color2);
+        int[] color1 = {255, 255, 255}; // White
+        int[] color2 = {98, 0, 237}; // Purpule used in app
+        int[] color3 = {0, 0, 0}; // Black
+        boolean result1 = authLogic.colorAccessibility(color1, color1); // White and White
+        boolean result2 = authLogic.colorAccessibility(color1, color2); // White and Purple (Colors used in app)
+        boolean result3 = authLogic.colorAccessibility(color1,color3); // White and Black
 
         // Assert that the result should be true since the contrast ratio is greater than 4.5
-        Assert.assertFalse(result);
+        Assert.assertFalse(result1);
+        Assert.assertTrue(result2);
+        Assert.assertTrue(result3);
     }
-
-
-
 }
 
 

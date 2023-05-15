350b0951e0685d71117eb3a5f9c7144bcb441efa
ArtLiathain <art.oliathain@gmail.com>
2023-05-15 10:32:07 +0100

    Moved tests to see if mocking is breaking it

--- app/src/test/java/com/example/kad/DateTimeTest.java
+++ app/src/test/java/com/example/kad/DateTimeTest.java
@@ -20,42 +20,6 @@ import org.mockito.Mockito;
 
 @RunWith(Enclosed.class)
 public class DateTimeTest {
-  @RunWith(Parameterized.class)
-  public static class HolidayApi {
-    private String day;
-
-    private boolean expectedValue;
-    DateTimeLogic dateTimeLogic;
-
-
-    public HolidayApi(String day, boolean expectedValue) {
-      this.day = day;
-      this.expectedValue = expectedValue;
-    }
-
-    @Before
-    public void setUp() throws Exception {
-      dateTimeLogic = new DateTimeLogic();
-    }
-
-    @Parameterized.Parameters
-    public static Collection dates() {
-      return Arrays.asList(
-          new Object[][] {
-            {"2022-12-26 10:00", true},
-            {"2022-08-20 10:00", false},
-            {"2022-12-10 09:00", false}
-          });
-    }
-
-    @Test
-    public void test_isHolidayApiWorking() {
-      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-      LocalDateTime formattedDay = LocalDateTime.parse(day, formatter);
-      assertEquals(dateTimeLogic.isHoliday(formattedDay), expectedValue);
-    }
-  }
-
 
   @RunWith(Parameterized.class)
   public static class StartTimeTests {

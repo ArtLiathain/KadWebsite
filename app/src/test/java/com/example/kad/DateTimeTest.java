f629be42c02bab64db91b9b6c8ff90b62a9d8f10
ArtLiathain <art.oliathain@gmail.com>
2023-05-15 11:16:17 +0100

    Test mocking

--- app/src/test/java/com/example/kad/DateTimeTest.java
+++ app/src/test/java/com/example/kad/DateTimeTest.java
@@ -82,9 +82,10 @@ public class DateTimeTest {
               {new String[]{"11:00"}, false, new String[]{"Not Selected",
                       "09:00 AM", "10:00 AM", "12:00 PM", "01:00 PM",
                       "02:00 PM", "03:00 PM", "04:00 PM"}},
+              {new String[]{"02:00", "03:00", "04:00"}, true, new String[]{"No Times Available"}},
               {new String[]{"02:00", "03:00", "04:00"}, false, new String[]{"Not Selected",
-                      "09:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "01:00 PM"}},
-              {new String[]{"02:00", "03:00", "04:00"}, true, new String[]{"No Times Available"}}
+                      "09:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "01:00 PM"}}
+
       });
       }
     @Test

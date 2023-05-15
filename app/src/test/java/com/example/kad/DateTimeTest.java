3e830cc162421883f2923c33da6494583d06ee15
ArtLiathain <art.oliathain@gmail.com>
2023-05-15 11:41:25 +0100

    See if mocking is ruining everything

--- app/src/test/java/com/example/kad/DateTimeTest.java
+++ app/src/test/java/com/example/kad/DateTimeTest.java
@@ -54,54 +54,5 @@ public class DateTimeTest {
     }
   }
 
-  @RunWith(Parameterized.class)
-  public static class StartTimeTests {
-    DateTimeLogic dateTimeLogic;
-    private String[] StartTimes;
-    private LocalDateTime dayOfBooking;
-    private String[] expectedOutput;
-    private boolean isDayOff;
-    DateTimeLogic a;
-    public StartTimeTests(String[] startTimes,boolean isDayOff, String[] expectedOutput) {
-      this.StartTimes = startTimes;
-      this.isDayOff = isDayOff;
-      this.dayOfBooking = LocalDateTime.now();
-      this.expectedOutput = expectedOutput;
-    }
 
-    @Before
-    public void setUp() throws Exception {
-      dateTimeLogic = new DateTimeLogic();
-
-    }
-    @Parameterized.Parameters
-    public static Collection availableTimes() {
-      return Arrays.asList(new Object[][] {
-              {new String[]{"10:00", "03:00", "01:00"}, false, new String[]{"Not Selected",
-                      "09:00 AM", "11:00 AM", "12:00 PM",
-                      "02:00 PM", "04:00 PM"}},
-              {new String[]{"11:00"}, false, new String[]{"Not Selected",
-                      "09:00 AM", "10:00 AM", "12:00 PM", "01:00 PM",
-                      "02:00 PM", "03:00 PM", "04:00 PM"}},
-              {new String[]{"02:00", "03:00", "04:00"}, true, new String[]{"No Times Available"}},
-              {new String[]{"02:00", "03:00", "04:00"}, false, new String[]{"Not Selected",
-                      "09:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "01:00 PM"}}
-
-      });
-      }
-    @Test
-    public void test_returnStartDates(){
-      ArrayList<LocalDateTime> StartTimesDates = new ArrayList<>();
-      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-M-dd HH:mm");
-      for (String time : StartTimes){
-        LocalDateTime times = LocalDateTime.parse(dayOfBooking.getYear() + "-" +
-                dayOfBooking.getMonthValue()+ "-" + dayOfBooking.getDayOfMonth()+ " " +
-                time, formatter);
-        StartTimesDates.add(times);
-      }
-      DateTimeLogic aSpy = Mockito.spy(dateTimeLogic);
-      Mockito.when(aSpy.isHoliday(dayOfBooking)).thenReturn(isDayOff);
-      assertArrayEquals(expectedOutput, aSpy.returnStartTimes(StartTimesDates,dayOfBooking));
-  }
-}
 }

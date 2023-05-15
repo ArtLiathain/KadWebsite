c91d396142857d6bbef9e6a1d7067e795d0a0988
ArtLiathain <art.oliathain@gmail.com>
2023-05-15 22:07:53 +0100

    FIX THE THING THANKS GPT

--- app/src/main/java/com/example/kad/activities/RoomBookingPageActivity.java
+++ app/src/main/java/com/example/kad/activities/RoomBookingPageActivity.java
@@ -2,8 +2,9 @@ package com.example.kad.activities;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,25 +12,23 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.Spinner;
 import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.kad.DateTimeLogic;
 import com.example.kad.FirebaseLogic;
-
 import com.example.kad.Generated;
 import com.example.kad.R;
 
 import java.time.LocalDateTime;
 
-
 public class RoomBookingPageActivity extends AppCompatActivity {
-
 
     Spinner hoursAvailable;
     DatePicker datePicker;
     Button buttonToRoomSelection;
     Button buttonToPlaceBooking;
     TextView textOfRoomInfo;
-
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -38,7 +37,7 @@ public class RoomBookingPageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.room_booking_page);
 
-//-------------------------Argument Passed Into TextView---------------------------
+        //-------------------------Argument Passed Into TextView---------------------------
         textOfRoomInfo = findViewById(R.id.textViewRoomInfo);
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
@@ -46,7 +45,7 @@ public class RoomBookingPageActivity extends AppCompatActivity {
             textOfRoomInfo.setText(passedArgument);
         }
 
-// ---------------------------------Back Button---------------------------------
+        // ---------------------------------Back Button---------------------------------
         buttonToRoomSelection = findViewById(R.id.backButtonRoomSelection);
         buttonToRoomSelection.setOnClickListener(new View.OnClickListener() {
             @Generated
@@ -57,8 +56,7 @@ public class RoomBookingPageActivity extends AppCompatActivity {
             }
         });
 
-
-// -----------------------------Place Booking Button---------------------------------
+        // -----------------------------Place Booking Button---------------------------------
         buttonToPlaceBooking = findViewById(R.id.placeBookingButton);
         buttonToPlaceBooking.setOnClickListener(new View.OnClickListener() {
             @Generated
@@ -69,41 +67,53 @@ public class RoomBookingPageActivity extends AppCompatActivity {
             }
         });
 
-
-// ---------------------------------Date Picker---------------------------------
-
+        // ---------------------------------Date Picker---------------------------------
         datePicker = findViewById(R.id.datePicker);
         datePicker.setOnDateChangedListener(new DatePicker.OnDateChangedListener() {
             @Override
             @Generated
             public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                // Do nothing
             }
         });
 
-
-// ---------------------------------Hours Available---------------------------------
+        // ---------------------------------Hours Available---------------------------------
         hoursAvailable = findViewById(R.id.dropdownTimeSelection);
-        // Retrieve a list of available times from a data sourc
-        DateTimeLogic dateTimeLogic = new DateTimeLogic();
-        FirebaseLogic firebaseLogic = new FirebaseLogic();
-        firebaseLogic.getDates(LocalDateTime.now(), 1);
-        ArrayAdapter ad = new ArrayAdapter(this, android.R.layout.simple_spinner_item,
-                dateTimeLogic.returnStartTimes(firebaseLogic.getStartDates(), LocalDateTime.now()));
-        // Styles dropdown of numbers nicely
-        ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        hoursAvailable.setAdapter(ad);
-// ------------------------------------------------------------------------
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                DateTimeLogic dateTimeLogic = new DateTimeLogic();
+                FirebaseLogic firebaseLogic = new FirebaseLogic();
+
+                firebaseLogic.getDates(LocalDateTime.now(), 1);
+
+                final ArrayAdapter<String> adapter = new ArrayAdapter<>(RoomBookingPageActivity.this, android.R.layout.simple_spinner_item,
+                        dateTimeLogic.returnStartTimes(firebaseLogic.getStartDates(), LocalDateTime.now()));
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                // Update the UI on the main thread using a Handler
+                new Handler(Looper.getMainLooper()).post(new Runnable() {
+                    @Override
+                    public void run() {
+                        hoursAvailable.setAdapter(adapter);
+                    }
+                });
+            }
+        }).start();
+
         hoursAvailable.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             @Generated
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // Do nothing
             }
+
             @Override
             @Generated
             public void onNothingSelected(AdapterView<?> parent) {
                 // Do nothing
             }
         });
-
     }
 }
